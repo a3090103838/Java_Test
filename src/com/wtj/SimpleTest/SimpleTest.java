cda12b4bcf8a5e006a89d9bb088e909d83b92b93
wangtianju <wangtianju@xiaomi.com>
2014-11-14 16:58:03 +0800

    update

--- src/com/wtj/SimpleTest/SimpleTest.java
+++ src/com/wtj/SimpleTest/SimpleTest.java
@@ -5,11 +5,7 @@ import java.util.Date;
 public class SimpleTest {
 
 	public static void main(String[] args) throws InterruptedException {
-		
-
-		System.out.print(new Date().getTime());
-		Thread.sleep(1000);
-		System.out.print(new Date().getTime());
+		System.out.print("[{\"content\":[{\"category\":\"mistat_default\",\"values\":[{\"value\":1,\"type\":\"event\",\"key\":\"event6\"},{\"value\":\"test\",\"type\":\"property\",\"key\":\"property3\"},{\"value\":87,\"type\":\"count\",\"key\":\"count5\"},{\"value\":1,\"type\":\"event\",\"key\":\"event4\"},{\"value\":45,\"type\":\"count\",\"key\":\"count6\"},{\"value\":9906,\"type\":\"numeric\",\"key\":\"numeric4\"},{\"value\":6277,\"type\":\"numeric\",\"key\":\"numeric3\"},{\"value\":7650,\"type\":\"numeric\",\"key\":\"numeric9\"},{\"value\":8,\"type\":\"count\",\"key\":\"count8\"},{\"value\":79,\"type\":\"count\",\"key\":\"count4\"}]},{\"category\":\"mistat_session\",\"values\":[{\"start\":1405576037241,\"end\":1405576043929,\"env\":\"mobile-umts-internet\"},{\"start\":1405575718477,\"end\":1405575995050,\"env\":\"mobile-umts-internet\"}]},{\"index\":\"主界面,二级界面\",\"category\":\"mistat_pv\",\"values\":[\"1\",\"1,2,1,2,1,2,1,2,1,2,1\"]}],\"endTS\":1405576115724,\"startTS\":1405575840875}]");
 	}
 
 }
